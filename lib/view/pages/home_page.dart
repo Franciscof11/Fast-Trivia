405d8ef4b47897612ce093a6392785f130e7bd4d
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-08-22 00:46:47 -0300

    refactor: ListView questionarios

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -17,78 +17,81 @@ class HomePage extends ConsumerWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Bem vindo!',
-                        style: GoogleFonts.nunito(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Bem vindo!',
+                          style: GoogleFonts.nunito(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Escolha um Quiz',
-                        style: GoogleFonts.nunito(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          'Escolha um Quiz',
+                          style: GoogleFonts.nunito(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Icon(
-                    Icons.menu,
-                    color: Colors.yellow[700],
-                    size: 32,
-                  )
-                ],
-              ),
-              //
-              const SizedBox(height: 40),
-              //
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.7,
-                width: MediaQuery.of(context).size.width,
-                child: questionarios.when(
-                  data: (data) {
-                    return RefreshIndicator(
-                      color: Colors.black,
-                      onRefresh: () async {
-                        // ignore: unused_result
-                        ref.refresh(questionariosProvider.future);
-                        return Future<void>.delayed(const Duration(seconds: 1));
-                      },
-                      child: ListView.separated(
-                        physics: const BouncingScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return CustomListTile(
-                            titulo: data[index].titulo,
-                            urlImageBanner: data[index].urlImageBanner,
-                          );
-                        },
-                        separatorBuilder: (context, index) => const Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 10),
-                          child: Divider(color: Colors.white, thickness: 1),
-                        ),
-                        itemCount: data.length,
-                      ),
-                    );
-                  },
-                  error: (error, _) => Center(
-                    child: Text('$error'),
-                  ),
-                  loading: () => const CircularProgressIndicator(),
+                      ],
+                    ),
+                    Icon(
+                      Icons.menu,
+                      color: Colors.yellow[700],
+                      size: 32,
+                    )
+                  ],
                 ),
-              ),
-            ],
+                //
+                const SizedBox(height: 40),
+                //
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  width: MediaQuery.of(context).size.width,
+                  child: questionarios.when(
+                    data: (data) {
+                      return RefreshIndicator(
+                        color: Colors.black,
+                        onRefresh: () async {
+                          // ignore: unused_result
+                          ref.refresh(questionariosProvider.future);
+                          return Future<void>.delayed(
+                              const Duration(seconds: 1));
+                        },
+                        child: ListView.separated(
+                          physics: const BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return CustomListTile(
+                              titulo: data[index].titulo,
+                              urlImageBanner: data[index].urlImageBanner,
+                            );
+                          },
+                          separatorBuilder: (context, index) => const Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 10),
+                            child: Divider(color: Colors.white, thickness: 1),
+                          ),
+                          itemCount: data.length,
+                        ),
+                      );
+                    },
+                    error: (error, _) => Center(
+                      child: Text('$error'),
+                    ),
+                    loading: () => const CircularProgressIndicator(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
