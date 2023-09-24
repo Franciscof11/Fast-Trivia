48bae1cc5e0fce4e0aa67ca17bda76dd626e95c1
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-09-24 13:59:51 -0300

    feat : add device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:fast_trivia/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MainApp(),
+    ProviderScope(
+      child: DevicePreview(
+        builder: (context) => const MainApp(),
+      ),
     ),
   );
 }
@@ -17,6 +20,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Fast Trivia',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       routerConfig: routes,
     );
--- lib/view/pages/quiz_page.dart
+++ lib/view/pages/quiz_page.dart
@@ -18,90 +18,91 @@ class QuizPage extends ConsumerWidget {
     final questoes = ref.watch(questoesProvider(idQuestionario));
 
     return questoes.when(
-      data: (data) => Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.yellow[700],
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 150),
-                child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadiusDirectional.only(
-                        topStart: Radius.circular(45),
-                        topEnd: Radius.circular(45),
-                      )),
-                ),
+      data: (data) => Scaffold(
+        backgroundColor: Colors.yellow[700],
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 110),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadiusDirectional.only(
+                      topStart: Radius.circular(45),
+                      topEnd: Radius.circular(45),
+                    )),
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 90),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () => context.pop(),
-                      child: const Icon(
-                        Icons.arrow_back_ios,
-                        size: 32,
-                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => context.pop(),
+                    child: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 32,
                     ),
-                    const SizedBox(width: 108),
-                    Text(
-                      '1 / 5',
-                      style: GoogleFonts.nunito(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  const SizedBox(width: 108),
+                  Text(
+                    '1 / 5',
+                    style: GoogleFonts.nunito(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24, top: 200),
-                child: Column(
-                  children: [
-                    Text(
-                      data.first.pergunta,
-                      style: GoogleFonts.nunito(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-                        wordSpacing: 6,
-                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24, top: 140),
+              child: Column(
+                children: [
+                  Text(
+                    data.first.pergunta,
+                    style: GoogleFonts.nunito(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                      wordSpacing: 6,
                     ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    CustomButtom(
-                        title: data.first.alternativas.first.titulo,
-                        onTap: () {}),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    CustomButtom(
-                        title: data.first.alternativas[1].titulo, onTap: () {}),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    CustomButtom(
-                        title: data.first.alternativas[2].titulo, onTap: () {}),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    CustomButtom(
-                        title: data.first.alternativas[3].titulo, onTap: () {}),
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  CustomButtom(
+                    title: data.first.alternativas[0].titulo,
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomButtom(
+                    title: data.first.alternativas[1].titulo,
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomButtom(
+                    title: data.first.alternativas[2].titulo,
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomButtom(
+                    title: data.first.alternativas[3].titulo,
+                    onTap: () {},
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       error: (error, _) => Center(
